702af8eab586cdd0bd3afa5361091c3d39b1ed13
mnurardhi <mn.ardhi@gmail.com>
2026-04-16 22:59:16 +0700

    Merubah Route untuk membuat Polisi Lalu Lintas (Gerbang utama setelah Login)

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-// 1. Rute Default / Mahasiswa (Bisa diakses siapa saja yang sudah login)
+// 1. POLISI LALU LINTAS (Gerbang Utama Setelah Login)
+Route::middleware(['auth', 'verified'])->get('/dashboard', function () {
+    // Jika yang login adalah admin, arahkan ke dasbor admin
+    if (auth()->user()->role === 'admin') {
+        return redirect()->route('admin.dashboard');
+    }
+    // Jika bukan admin (mahasiswa), arahkan ke dasbor mahasiswa
+    return redirect()->route('mahasiswa.dashboard');
+})->name('dashboard');
+
+// 2. RUTE KHUSUS MAHASISWA
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', \App\Livewire\MahasiswaDashboard::class)->name('dashboard');
+    Route::get('/mahasiswa-dashboard', \App\Livewire\MahasiswaDashboard::class)->name('mahasiswa.dashboard');
     Route::get('/pendaftaran', \App\Livewire\FormPendaftaran::class)->name('pendaftaran');
 });
 
-// 2. Rute Khusus Eksekutif Rektorat (Digembok oleh Middleware 'admin')
+// 3. RUTE KHUSUS EKSEKUTIF REKTORAT (ADMIN)
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin-dashboard', \App\Livewire\AdminDashboard::class)->name('admin.dashboard');
     Route::get('/admin-pendaftar', \App\Livewire\AdminPendaftar::class)->name('admin.pendaftar');
